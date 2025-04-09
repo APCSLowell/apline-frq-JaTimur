877a68ce6af2eccbc35b7e8c41f2105b5b48548e
JaTimur <JaTimur@s.sfusd.edu>
2025-04-08 20:53:16 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,14 @@
-public class APLine
-{
-  /* your code here */
-  
+public class APLine{
+  int myA,myB,myC;
+  public APLine(int a,int b,int c){
+    myA=a;
+    myB=b;
+    myC=c;
+  }
+  public double getSlope(){
+    return (double)((a*-1)/b);
+  }
+  public boolean isOnLine(int x,int y){
+    return (myA*x)+(myB*y)+myC==0;
+  }
 }
